8d1ab18dafafcb1f58fea7a2c1feeda68a8b48f5
juan <109624079+juannotdev@users.noreply.github.com>
2022-08-07 20:19:12 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Juan'
+description 'FiveM Basic RP Chat Script'
+
+client_script "client/client.lua"
+shared_script 'config.lua'
+server_script "server/server.lua"
