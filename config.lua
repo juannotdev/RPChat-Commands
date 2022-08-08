123e83783b4866078c6cbdbe6864d9244545e6ea
juan <109624079+juannotdev@users.noreply.github.com>
2022-08-07 20:18:50 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+EnableDiscordCommand = true -- Set to true if you want /discord enabled. 
+EnableWebsiteCommand = true -- Set to true if you want /website enabled.
+EnableRPChat = true -- Set to true if you want the RP Chat enabled (/me, /do, /twt, etc.)
+
+DiscordLink = 'discord.gg/placeholder'
+WebsiteLink = 'www.website.com'
